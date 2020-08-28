93de92cab6a68383b23e9751e99b98c7491d1d76
cynthialu42 <clu2334@gmail.com>
2020-08-28 17:06:28 -0500

    Updates

---
+++ precache-manifest.de776fe3e1bfba5a6411cd454ae6b4bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b2e5148bdef3711efe430715365074",
+    "url": "/flood-it/index.html"
+  },
+  {
+    "revision": "30d2cdaa93f508a4228b",
+    "url": "/flood-it/static/css/main.d782961a.chunk.css"
+  },
+  {
+    "revision": "d920b4304adcd7663ff7",
+    "url": "/flood-it/static/js/2.56b2f821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flood-it/static/js/2.56b2f821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d2cdaa93f508a4228b",
+    "url": "/flood-it/static/js/main.a201596b.chunk.js"
+  },
+  {
+    "revision": "815fd4bc6a88a4fe430a",
+    "url": "/flood-it/static/js/runtime-main.c362ae74.js"
+  }
+]);
